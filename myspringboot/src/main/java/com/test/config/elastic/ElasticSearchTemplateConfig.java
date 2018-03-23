30a82490627ae1f3303e7c7b936cdff6956d0f41
xueyongjun <yongjun.xue@autozi.com>
2018-03-23 18:07:47 +0800

    elasticsearch操作的方法 1.TransportClient(不推荐) 2.ElasticsearchTemplate 3.PagingAndSortingRepository的实现类

--- myspringboot/src/main/java/com/test/config/elastic/ElasticSearchTemplateConfig.java
+++ myspringboot/src/main/java/com/test/config/elastic/ElasticSearchTemplateConfig.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 
+
+/**
+ * 操作elasticsearch可以使用ElasticsearchTemplate，也可以使用PagingAndSortingRepository的实现类
+ * @author xueyongjun
+ *
+ */
 @Configuration
 public class ElasticSearchTemplateConfig {
 	
--- myspringboot/src/main/java/com/test/repositoty/elastic/UserRepository.java
+++ myspringboot/src/main/java/com/test/repositoty/elastic/UserRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.test.pojo.elastic.User;
 
+/**
+ * UserRepository的使用需要用到ElasticsearchTemplate的实例，所以在ElasticSearchTemplateConfig对ElasticsearchTemplate进行了初始化
+ * @author xueyongjun
+ *
+ */
 public interface UserRepository extends PagingAndSortingRepository<User, Long>{
 
 }
